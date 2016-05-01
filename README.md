6234991ea491da6fb44cb7233eefda532d9ada5b
Zackary BEAUGELIN <zackary.beaugelin@epitech.eu>
2016-05-01 16:49:28 +0200

    Install script + 'manual' in the README.md

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ The structure allows the easy and quick change of each of these components.
 In addition, there are two game modes:
 + Player VS Player: game mode allows a human to play against another human. You will need to provide a customized set of aid for each player offering a coup considered interesting.
 + Player VS CPU: The most interesting game mode, in which the human player plays against artificial intelligence.
+
+## Install script
+Use:
+```bash
+./install.sh
+```
+to run the script.
+
+```bash
+[I]nstall dependencies/[S]etup/[E]xit?
+```
++ `Setup` will perform the configuration of virtualenv and put you in the good environment.
++ `Install dependencies` Will install all dependencies for `python3` `pip3` and `pygames`
++ `Exit` obviously exit the script.
--- install.sh
+++ install.sh
@@ -1,31 +1,44 @@
-#!/bin/sh
+#!/bin/bash
+
+function install {
+    # Install dependencies
+    sudo apt-get install mercurial python3-dev python3-numpy python3-pip libav-tools \
+	 libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
+	 libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
+
+    sudo pip install virtualenv
+    sudo pip install virtualenvwrapper
+    mkdir ~/virtualenvs
+    export WORKON_HOME=~/virtualenvs
+    source /usr/local/bin/virtualenvwrapper.sh
+    export PIP_VIRTUALENV_BASE=~/virtualenvs
+    source ~/.bashrc
+}
 
 function setup {
-  # Install dependencies
-  sudo apt-get install mercurial python3-dev python3-numpy libav-tools \
-      libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
-      libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
+    # Grab source
+    hg clone https://bitbucket.org/pygame/pygame
 
-  # Grab source
-  hg clone https://bitbucket.org/pygame/pygame
+    # Build pygame
+    cd pygame
+    python3 setup.py build
 
-  # Build pygame
-  cd pygame
-  python3 setup.py build
+    # Create and activate the virtual environment
+    mkvirtualenv Gomoku-beauge_z --python=/usr/bin/python3
+    workon Gomoku-beauge_z
+    echo 'Virtualenv name: Gomoku-beauge_z'
 
-  # Create and activate the virtual environment
-  mkvirtualenv Gomoku-beauge_z --python=/usr/bin/python3
-  workon Gomoku-beauge_z
-
-  # Install into the virtual environment
-  python3 setup.py install --prefix="$HOME/.virtualenvs/Gomoku-beauge_z"
+    # Install into the virtual environment
+    python3 setup.py install --prefix="$HOME/.virtualenvs/Gomoku-beauge_z"
 }
 
 while true; do
-    read -p "WARNING: To install pygame into a virtual environment it is recommended to first install and configure virtualenvwrapper. Continue? [Y/N]" yn
+    echo "WARNING: To install pygame into a virtual environment it is recommended to first install and configure virtualenvwrapper."
+    read -p "[I]nstall dependencies/[S]etup/[E]xit?" yn
     case $yn in
-        [Yy]* ) setup; break;;
-        [Nn]* ) exit;;
+        [Ss]* ) setup; break;;
+        [Ii]* ) install; setup; break;;
+        [Ee]* ) exit;;
         * ) clear ;;
     esac
 done
